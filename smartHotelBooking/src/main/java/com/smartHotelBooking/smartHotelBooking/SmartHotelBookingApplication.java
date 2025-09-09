1ef3de9e90be5ca7bd58fc4ee31320749c386bb3
Sharma <2440654@cognizant.com>
2025-09-09 17:00:22 +0530

    Code Skeleton Added

---
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/SmartHotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.smartHotelBooking.smartHotelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHotelBookingApplication.class, args);
+	}
+
+}
---
+++ smartHotelBooking/src/test/java/com/smartHotelBooking/smartHotelBooking/SmartHotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartHotelBooking.smartHotelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
