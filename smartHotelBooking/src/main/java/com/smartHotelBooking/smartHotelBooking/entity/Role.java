cffd671d8f0ba3acbc44e66acfd0854b1530ad03
Aayush2440654 <Aayush.Sharma3@cognizant.com>
2025-09-09 20:59:12 +0530

    User entity and Roles added

---
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Role.java
@@ -0,0 +1,7 @@
+package com.smartHotelBooking.smartHotelBooking.entity;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    USER
+}
--- smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/User.java
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/User.java
@@ -1,5 +1,19 @@
 package com.smartHotelBooking.smartHotelBooking.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class User {
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+    private String contactNumber;
+
 
 }
