6eadd20dada9b14917da3e800e4831f58fbfbd15
Anurag2440835 <AnuragSanjay.Jadhav@cognizant.com>
2025-09-10 16:31:25 +0530

    Entity and Mappings added for Hotel and Room

--- smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Room.java
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Room.java
@@ -1,16 +1,31 @@
 package com.smartHotelBooking.smartHotelBooking.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Room {
+    @Column(name = "roomId")
     private Long roomId;
-    private Long hotelId;
+//    @Column(name = "hotelId",nullable = false)
+//    private Long hotelId;
+    @Column(name = "type",nullable = false)
     private String type;
+    @Column(name = "price",nullable = false)
     private Double price;
+    @Column(name = "availability",nullable = false)
     private Boolean availability;
+    @Column(name = "features")
     private String features;
+
+    @ManyToOne
+    @JoinColumn(name = "hotelId")
+    private Hotel hotelId;
 }
