55855920bf114a7f1649b7c905090177020c35a2
Shobhit <shobhit.agrawal@cognizant.com>
2025-09-11 17:21:20 +0530

    mapping annotation added in booking and payment

--- smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Booking.java
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Booking.java
@@ -1,5 +1,6 @@
 package com.smartHotelBooking.smartHotelBooking.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,33 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookingId")
     private Long bookingId;
-    private Long userId;
+
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Column(name = "roomId", nullable = false)
     private Long roomId;
+
+    @Column(name = "checkInDate", nullable = false)
     private LocalDate checkInDate;
+
+    @Column(name = "checkOutDate", nullable = false)
     private LocalDate checkOutDate;
+
+    @Column(name = "bookingDate", nullable = false)
     private LocalDate bookingDate;
+
+    @Column(name = "status", nullable = false)
     private String status;
+
+    @ManyToOne
+    private User userId;
 }
+
--- smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Payment.java
+++ smartHotelBooking/src/main/java/com/smartHotelBooking/smartHotelBooking/entity/Payment.java
@@ -1,5 +1,6 @@
 package com.smartHotelBooking.smartHotelBooking.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,34 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "paymentId")
     private Long paymentId;
-    private Long bookingId;
+
+
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @JoinColumn(name = "bookingId", nullable = false)
+    private Booking booking;
+
+    @Column(name = "amount", nullable = false)
     private Double amount;
+
+    @Column(name = "paymentDate", nullable = false)
     private LocalDateTime paymentDate;
+
+    @Column(name = "paymentMethod", nullable = false)
     private String paymentMethod;
+
+    @Column(name = "paymentStatus", nullable = false)
     private String paymentStatus;
+
+    @ManyToOne
+    private User userId;
 }
 
